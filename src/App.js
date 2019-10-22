2da5fca59156a62cfa3028b82b92cb450d759cec
Fábio Ferreira <ffcfpten@gmail.com>
2019-10-22 21:24:33 +0100

    feat: simple input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
+
+// Logo
 import logo from './logo.svg';
+// Styles
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <p>Wanna save that cool video from your favorite Youtuber so you can watch it offline? Go ahead. :)</p>
+          <input type="text" name="videourl"></input>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
